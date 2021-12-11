c6ff4bd26607f7d96e88150ceacfddd27dcb6210
neqelovek <neqelovek@gmail.com>
2021-12-11 17:47:46 +0300

    Button finish

--- app/src/main/java/ru/gb/homework/MainActivity.java
+++ app/src/main/java/ru/gb/homework/MainActivity.java
@@ -5,13 +5,23 @@ package ru.gb.homework;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView nameText;
+    private Button clickButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        nameText = findViewById(R.id.name_text_view);
+        clickButton = findViewById(R.id.name_Button);
+
+        clickButton.setOnClickListener(v -> nameText.setText("Cool"));
     }
 }
